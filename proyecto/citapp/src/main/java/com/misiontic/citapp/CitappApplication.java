04da2370aa577ee9f49c8934bf9d1df50a2992b4
Fer <luferbrica@gmail.com>
2021-12-05 17:38:17 -0500

    Creo proyecto en SpringBoot

---
+++ proyecto/citapp/src/main/java/com/misiontic/citapp/CitappApplication.java
@@ -0,0 +1,13 @@
+package com.misiontic.citapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitappApplication.class, args);
+	}
+
+}
